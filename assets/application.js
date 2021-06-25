ef2f696a2ec5bee88fce45f3b43311b3f323fb77
ahmedezzaouia <35857662+ahmedezzaouia@users.noreply.github.com>
2021-06-25 21:52:57 +0100

    finish cart and home page

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,14 @@
-// Put your applicaiton javascript here
+$(document).ready(function () {
+  $(window).resize(function () {
+    if ($(window).width() >= 980) {
+      $(".navbar .dropdown-toggle").hover(function () {
+        $(this).parent().toggleClass("show");
+        $(this).parent().find(".dropdown-menu").toggleClass("show");
+      });
+
+      $(".navbar .dropdown-menu").mouseleave(function () {
+        $(this).removeClass("show");
+      });
+    }
+  });
+});
